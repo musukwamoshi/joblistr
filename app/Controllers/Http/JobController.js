0e1fc435bf76e32dff45cbb93e4f215bf9930580
Moshi Musukwa <musukwamoshi@gmail.com>
2018-09-18 01:32:47 +0200

    first commit for an adonis js app

---
+++ app/Controllers/Http/JobController.js
@@ -0,0 +1,20 @@
+'use strict'
+
+const Job = use('App/Models/Job');
+
+class JobController {
+
+    async home({ request, view, response, auth }) {
+
+        //index of application
+
+
+
+    }
+
+
+
+
+}
+
+module.exports = JobController
