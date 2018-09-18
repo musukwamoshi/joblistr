0210a5c995f60feee2b71d68a24f6cdab7d65e5e
Moshi Musukwa <musukwamoshi@gmail.com>
2018-09-18 02:13:17 +0200

    Fleshed out index function

--- app/Controllers/Http/JobController.js
+++ app/Controllers/Http/JobController.js
@@ -4,11 +4,23 @@ const Job = use('App/Models/Job');
 
 class JobController {
 
-    async home({ request, view, response, auth }) {
+    //function that pulls all jobs
+    async home({ view }) {
 
-        //index of application
+        //create new job
+        const job = new Job;
+        job.title = "Job One";
+        job.link = "findjobs.com";
+        job.description = "Just a run of the mill job";
 
+        //save job
+        await job.save();
 
+        //fetch job
+        const jobs = await Job.all();
+
+        //return jobs with async
+        return view.render('view.index', { jobs: jobs.toJSON() });
 
     }
 
